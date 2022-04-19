d8f07cef4f4885c679594d650b24f6123834a78f
almrttsn <88113243+almrttsn@users.noreply.github.com>
2022-04-19 19:14:43 +0300

    player colour changes implemented

--- Assets/Scripts/PlayerDetector.cs
+++ Assets/Scripts/PlayerDetector.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,20 +14,22 @@ public class PlayerDetector : MonoBehaviour
     private PlayerController _playerController;
 
     private int _count;
+
     public void Initialize(PlayerController playerController)
     {
         {
             _playerController = playerController;
             var prop = Instantiate(_parentPrefab, new Vector3(0, 0, 0), Quaternion.identity);
             _initializedDesiredObjects = prop;
+
         }
     }
 
     private void Update()
     {
         _parentPrefab.transform.GetChild(2).gameObject.SetActive(true);
+        ChangeMode();
     }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Luck" && this.tag == "Player")
@@ -40,15 +43,50 @@ public class PlayerDetector : MonoBehaviour
             Debug.Log(_count);
         }
     }
+    private void ChangeMode()
+    {
+        if (_count == -2)
+        {
+            var _current = _parentPrefab.transform.GetChild(1);
+            _current.gameObject.SetActive(false);
 
-    //private void ChangeThePlayer()
-    //{
-    //    if(_count > 2)
-    //    {
-    //        var a =_parentPrefab.transform.GetChild(3);
-    //        a.gameObject.SetActive(true);
-    //    }
-    //}
+            var _previous = _parentPrefab.transform.GetChild(0);
+            _previous.gameObject.SetActive(true);
+        }
+        if (_count == -1)
+        {
+            var _current = _parentPrefab.transform.GetChild(2);
+            _current.gameObject.SetActive(false);
+
+            var _previous = _parentPrefab.transform.GetChild(1);
+            _previous.gameObject.SetActive(true);
+        }
+        if (_count == 0)
+        {
+            var _current = _parentPrefab.transform.GetChild(1);
+            _current.gameObject.SetActive(true);
+
+            var _next = _parentPrefab.transform.GetChild(2);
+            _next.gameObject.SetActive(true);
+        }
+        if (_count == 1)
+        {
+            var _current = _parentPrefab.transform.GetChild(2);
+            _current.gameObject.SetActive(false);
+
+            var _next = _parentPrefab.transform.GetChild(3);
+            _next.gameObject.SetActive(true);
+        }
+
+        if (_count == 2)
+        {
+            var _current = _parentPrefab.transform.GetChild(3);
+            _current.gameObject.SetActive(false);
+
+            var _next = _parentPrefab.transform.GetChild(4);
+            _next.gameObject.SetActive(true);
+        }
+    }
 
 
 }
