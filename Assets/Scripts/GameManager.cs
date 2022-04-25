f3cb09655b91997ff79bbe562dbac4803810329f
almrttsn <88113243+almrttsn@users.noreply.github.com>
2022-04-25 12:39:24 +0300

    new levels created

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private LevelBehaviour _level;
+    [SerializeField] List<LevelBehaviour> _levels;
+    //[SerializeField] private LevelBehaviour _level;
     private LevelBehaviour _currentlyPlayingLevelBehaviour;
+    private LevelBehaviour _nextLevel;
 
     private bool _lastScore;
 
@@ -13,14 +15,27 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        _currentlyPlayingLevelBehaviour = Instantiate(_level);
+        _currentlyPlayingLevelBehaviour = Instantiate(_levels[0]);
         _currentlyPlayingLevelBehaviour.Initialize(this);
         InGamePanel.Initialize(this);
     }
     public void RestartLevel()
     {
         Destroy(_currentlyPlayingLevelBehaviour.gameObject);
-        _currentlyPlayingLevelBehaviour = Instantiate(_level);
+        _currentlyPlayingLevelBehaviour = Instantiate(_levels[0]);
         _currentlyPlayingLevelBehaviour.Initialize(this);
     }
+
+    public void LoadNextLevel()
+    {
+        for(int i = 1; i < _levels.Count; i++)
+        {
+            Destroy(_currentlyPlayingLevelBehaviour.gameObject);
+            _nextLevel = Instantiate(_levels[i]);
+            _nextLevel.Initialize(this);
+            return;
+        }
+    }
+
+
 }
