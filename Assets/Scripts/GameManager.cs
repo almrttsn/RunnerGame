1a6ee3386239b9a63ea39076654bfd52b0cc6dbb
almrttsn <88113243+almrttsn@users.noreply.github.com>
2022-04-25 22:16:57 +0300

    changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,30 +5,27 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] List<LevelBehaviour> _levels;
-    private LevelBehaviour _startingLevelBehaviour;
-    private LevelBehaviour _currentlyPlayingLevelBehaviour;
-    private LevelBehaviour _nextlyPlayingLeveBehaviour;
+    [SerializeField] LevelBehaviour _currentlyPlayingLevelBehaviour;
     public InGamePanel InGamePanel;
+    private int _currentIndex;
 
     private void Start()
     {
-        _currentlyPlayingLevelBehaviour = Instantiate(_levels[0]);
+        _currentlyPlayingLevelBehaviour = Instantiate(_currentlyPlayingLevelBehaviour);
         _currentlyPlayingLevelBehaviour.Initialize(this);
         InGamePanel.Initialize(this);
     }
     public void RestartLevel()
     {
         Destroy(_currentlyPlayingLevelBehaviour.gameObject);
-        _currentlyPlayingLevelBehaviour = Instantiate(_levels[]);
+        _currentlyPlayingLevelBehaviour = Instantiate(_currentlyPlayingLevelBehaviour);
         _currentlyPlayingLevelBehaviour.Initialize(this);
         InGamePanel.DisableShopPanel();
     }
 
     public void LoadNextLevel()
     {
-        Destroy(_currentlyPlayingLevelBehaviour.gameObject);
-        _nextlyPlayingLeveBehaviour = Instantiate(_levels[]);
-        _nextlyPlayingLeveBehaviour.Initialize(this);
+        Destroy(_currentlyPlayingLevelBehaviour.gameObject);        
         InGamePanel.DisableShopPanel();
     }
 
